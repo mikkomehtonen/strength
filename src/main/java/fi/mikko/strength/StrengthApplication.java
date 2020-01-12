cccaf480ae2434734002f981373e50c73dea59ac
Mikko Mehtonen <mikko.mehtonen@vincit.fi>
2020-01-12 09:25:38 +0200

    Initial spring boot project

---
+++ src/main/java/fi/mikko/strength/StrengthApplication.java
@@ -0,0 +1,13 @@
+package fi.mikko.strength;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrengthApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(StrengthApplication.class, args);
+  }
+
+}
---
+++ src/test/java/fi/mikko/strength/StrengthApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.mikko.strength;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrengthApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
